97409b605f14f56660947b4d89affb0a9520f662
doogoo <760625325@qq.com>
2016-07-12 15:48:15 +0800

    修正路径空间判断关系中的距离关系方法

--- app/src/main/java/dong/lan/mapeye/model/Line.java
+++ app/src/main/java/dong/lan/mapeye/model/Line.java
@@ -1,8 +1,11 @@
 package dong.lan.mapeye.model;
 
+import android.util.Log;
+
 import com.baidu.mapapi.model.LatLng;
 import com.baidu.mapapi.utils.DistanceUtil;
 
+import dong.lan.mapeye.BuildConfig;
 import io.realm.RealmObject;
 
 /**
@@ -35,20 +38,38 @@ public class Line extends RealmObject {
             B = start.lat - end.lat;
             C = end.lat * start.lng - start.lat * end.lng;
         }
+        if (BuildConfig.DEBUG) Log.d("Line", "A,B,C:" + A + "," + B + "," + C);
     }
 
 
+    public double getYuanxinjiao(double k) {
+        return 2 *Math.toDegrees( Math.asin(k / (2 * 6371004)));
+    }
+
+    public double getPointDis(double lat,double lng){
+        if(lat == start.lat)
+            return Math.abs(lng - start.lng);
+        else if(lng == start.lng)
+            return  Math.abs(lat - start.lat);
+        else{
+            return Math.sqrt((lat - start.lat)*(lat - start.lat) + (lng - start.lng)*(lng - start.lng));
+        }
+    }
+
     public double getDistance(double lat, double lng) {
-        return Math.abs(A * lat + B * lng + C) / Math.sqrt(A * A + B * B);
+        double d = Math.abs(A * lat + B * lng + C) / Math.sqrt(A * A + B * B);
+        d = d * DistanceUtil.getDistance(new LatLng(start.lat,start.lng),new LatLng(lat,lng))/getPointDis(lat,lng);
+        if (BuildConfig.DEBUG) Log.d("Line", "d:" + d);
+        return d;
     }
 
     public boolean isOnline(double lat, double lng) {
 
-        if(lat * A + lng * B + C == lng)
+        if (lat * A + lng * B + C == lng)
             return true;
-        else if(DistanceUtil.getDistance(new LatLng(lat, lng), new LatLng(start.lat, start.lng)) < Route.DIS)
+        else if (DistanceUtil.getDistance(new LatLng(lat, lng), new LatLng(start.lat, start.lng)) < Route.DIS)
             return true;
-        else if(getDistance(lat, lng)<Route.DIS)
+        else if (getDistance(lat, lng) < Route.DIS)
             return true;
         return false;
     }
