19d0595d3c2b2c7c1cfe5c5bc999a9781ba35f27
donlan <760625325@qq.com>
2017-04-07 12:36:48 +0800

    draw record trace to map

--- app/src/main/java/dong/lan/mapeye/views/MonitorRecordDetailActivity.java
+++ app/src/main/java/dong/lan/mapeye/views/MonitorRecordDetailActivity.java
@@ -20,7 +20,9 @@
 
 package dong.lan.mapeye.views;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.baidu.mapapi.map.BaiduMap;
@@ -35,24 +37,28 @@ import dong.lan.mapeye.R;
 import dong.lan.mapeye.model.MonitorRecode;
 import dong.lan.mapeye.model.TraceLocation;
 import dong.lan.mapeye.utils.MapUtils;
+import dong.lan.mapeye.views.customsView.DotView;
 import io.realm.Realm;
 
-public class MonitorRecordDetailActivity extends BaseActivity  {
+public class MonitorRecordDetailActivity extends BaseActivity {
 
     public static final String KEY_MONITOR_RECORD_ID = "monitorRecordId";
     public static final String KEY_POSITION = "position";
     @BindView(R.id.mapView)
     MapView mapView;
+    @BindView(R.id.bar_right)
+    TextView barRight;
+    View dot;
     private long id = 0;
     private int position;
+    private boolean hadDelete = false;
+    private BaiduMap baiduMap;
+    private ArrayList<TraceLocation> traceLocations;
 
     @OnClick(R.id.bar_left)
     void back() {
         finish();
     }
-
-    @BindView(R.id.bar_right)
-    TextView barRight;
 
     @OnClick(R.id.bar_right)
     void deleteMonitorRecord() {
@@ -65,12 +71,17 @@ public class MonitorRecordDetailActivity extends BaseActivity  {
 
         realm.commitTransaction();
         realm.close();
-        setResult(position);
+        hadDelete = true;
         finish();
     }
 
-    private BaiduMap baiduMap;
-    private ArrayList<TraceLocation> traceLocations;
+    @Override
+    public void finish() {
+        if (!hadDelete)
+            position = -1;
+        setResult(position);
+        super.finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,28 +92,39 @@ public class MonitorRecordDetailActivity extends BaseActivity  {
     }
 
     private void checkIntent() {
-        baiduMap = mapView.getMap();
         barRight.setText("删除");
+        new DotView(this, 5, Color.RED);
+        baiduMap = mapView.getMap();
         if (getIntent().hasExtra(KEY_MONITOR_RECORD_ID)) {
             id = getIntent().getLongExtra(KEY_MONITOR_RECORD_ID, 0);
             position = getIntent().getIntExtra(KEY_POSITION, -1);
             if (id != 0) {
                 Realm realm = Realm.getDefaultInstance();
+                realm.beginTransaction();
                 MonitorRecode monitorRecode = realm.where(MonitorRecode.class)
                         .equalTo("id", id).findFirst();
-                if (monitorRecode != null) {
-                    if (monitorRecode.getLocations() == null || monitorRecode.getLocations().isEmpty()) {
-                        toast("此记录没有监控记录点");
-                    } else {
-                        MapUtils.drawTrace(baiduMap,monitorRecode.getLocations(), BitmapDescriptorFactory.fromResource(R.drawable.dot_32_red));
-                    }
-                }
-
+                if (monitorRecode != null && monitorRecode.getLocations() != null)
+                    drawTrace(realm.copyFromRealm(monitorRecode));
+                realm.commitTransaction();
             }
         }
+
+
     }
 
+    private void drawTrace(final MonitorRecode monitorRecode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (monitorRecode.getLocations() == null || monitorRecode.getLocations().isEmpty()) {
+                    toast("此记录没有监控记录点");
+                } else {
+                    MapUtils.drawTrace(baiduMap, monitorRecode.getLocations(), BitmapDescriptorFactory.fromView(dot));
+                }
+            }
+        });
 
+    }
 
 
     @Override
